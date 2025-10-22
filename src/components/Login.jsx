77b9255361688cf1d9f039cc2507b673d16471c5
junaid <junaid.vjunu9895@gmail.com>
2025-10-22 18:03:23 +0530

    Updated App, Navbar, index.css, and Added Login, Sidebar Components

---
+++ src/components/Login.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onSubmitHandler = async (e) =>{
+        try {
+            e.preventDefault()
+            console.log(email, password);
+            
+        } catch (error) {
+            
+        }
+    }
+
+  return (
+    <div className="min-h-screen flex items-center justify-center w-full">
+      <div className="bg-whtie shadow-md rounded-lg px-8 py-6 max-w-md ">
+        <h1 className="text-2xl font-bold mb-4">Admin Panel</h1>
+        <form onSubmit={onSubmitHandler} action="">
+          <div className="mb-3 min-w-72">
+            <p className="text-sm font-medium text-gray-700 mb-2">
+              Email Address
+            </p>
+            <input
+              onChange={(e) =>setEmail(e.target.value)}
+              value={email}
+              className="rounded-md placeholder-gray-400 w-full px-3 py-2 border border-gray-300 outline-none"
+              type="email"
+              placeholder="your@email.com"
+              required
+            />
+          </div>
+          <div className="mb-3 min-w-72">
+            <p className="text-sm font-medium text-gray-700 mb-2">Password</p>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              className="rounded-md placeholder-gray-400 w-full px-3 py-2 border border-gray-300 outline-none"
+              type="password"
+              placeholder="Enter Your Password"
+              required
+            />
+          </div>
+          <button className="mt-2 w-full py-2 px-4 rounded-md text-white bg-black" type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
