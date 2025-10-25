0c5897bc4d1cdc35a48c46ed3767113bc4490d2c
junaid <junaid.vjunu9895@gmail.com>
2025-10-25 18:01:24 +0530

    Add.jsx, List.jsx Layout completed..

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
 import { assets } from '../assets/assets'
+import axios from 'axios'
+import { backendUrl } from '../App'
+import { toast } from 'react-toastify'
 
-const Add = () => {
+const Add = ({token}) => {
 
 
   const [image1, setImage1] = useState(false)
@@ -12,13 +15,57 @@ const Add = () => {
   const [name, setName] = useState("")
   const [description, setDescription] = useState("")
   const [price, setPrice] = useState("")
-  const [Category, setCategory] = useState("")
+  const [category, setCategory] = useState("")
   const [subCategory, setSubCategory]= useState("")
   const [bestseller, setBestseller] = useState(false)
   const [sizes, setSizes] = useState([])
 
+
+  const onSubmitHandler = async (e) =>{
+    e.preventDefault();
+
+    try {
+
+      const formData = new FormData()
+
+      formData.append("name", name)
+      formData.append("description", description)
+      formData.append("price", price)
+      formData.append("category", category)
+      formData.append("subCategory", subCategory)
+      formData.append("bestseller", bestseller)
+      formData.append("sizes", JSON.stringify(sizes))
+
+      image1 && formData.append("image1", image1)
+      image2 && formData.append("image2", image2)
+      image3 && formData.append("image3", image3)
+      image4 && formData.append("image4", image4)
+
+      const response = await axios.post(backendUrl + "/api/product/add", formData, {headers:{token} })
+
+      console.log(response.data);
+      
+
+      if(response.data.success){
+        toast.success(response.data.message)
+        setName('')
+        setDescription('')
+        setImage1(false)
+        setImage2(false)
+        setImage3(false)
+        setImage4(false)
+        setPrice('')
+      }else{
+        toast.error(response.data.message)
+      }
+      
+    } catch (error) {
+      
+    }
+  }
+
   return (
-    <form className='flex flex-col w-full items-start gap-3'>
+    <form onSubmit={onSubmitHandler} className='flex flex-col w-full items-start gap-3'>
       <div>
         <p>Upload Image</p>
 
@@ -56,7 +103,8 @@ const Add = () => {
 
         <div >
           <p className='mb-2'>Product Category</p>
-          <select onChange={(e) => setCategory(e.target.value)} value={Category} className='w-full px-3 py-2' name="" id="">
+          <select onChange={(e) => setCategory(e.target.value)} value={category} className='w-full px-3 py-2' name="" id="">
+            <option value="">Select</option>
             <option value="Men">Men</option>
             <option value="Women">Women</option>
             <option value="Kids">Kids</option>
@@ -66,6 +114,7 @@ const Add = () => {
         <div>
           <p className='mb-2'>Sub Category</p>
           <select onChange={(e) => setSubCategory(e.target.value)} value={subCategory} className='w-full px-3 py-2' name="" id="">
+            <option value="">Select</option>
             <option value="Topwear">Topwear</option>
             <option value="Bottomwear">Bottomwear</option>
             <option value="Winterwear">Winterwear</option>
@@ -84,30 +133,30 @@ const Add = () => {
         <p className='mb-2'>Product Sizes</p>
         <div className='flex gap-3'>
 
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>S</p>
+          <div onClick={() =>setSizes(prev => prev.includes("S")? prev.filter(item => item !== "S"): [...prev,"S"])}>
+            <p className={`${sizes.includes("S")? "bg-pink-100" : "bg-slate-200"} px-3 py-1 cursor-pointer`}>S</p>
           </div>
 
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>M</p>
+          <div onClick={() =>setSizes(prev => prev.includes("M")? prev.filter(item => item !== "M"): [...prev,"M"])}>
+            <p className={`${sizes.includes("M")? "bg-pink-100" : "bg-slate-200"} px-3 py-1 cursor-pointer`}>M</p>
           </div>
 
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>L</p>
+          <div onClick={() =>setSizes(prev => prev.includes("L")? prev.filter(item => item !== "L"): [...prev,"L"])}>
+            <p className={`${sizes.includes("L")? "bg-pink-100" : "bg-slate-200"} px-3 py-1 cursor-pointer`}>L</p>
           </div>
 
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XL</p>
+          <div onClick={() =>setSizes(prev => prev.includes("XL")? prev.filter(item => item !== "XL"): [...prev,"XL"])}>
+            <p className={`${sizes.includes("XL")? "bg-pink-100" : "bg-slate-200"} px-3 py-1 cursor-pointer`}>XL</p>
           </div>
 
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XXL</p>
+          <div onClick={() =>setSizes(prev => prev.includes("XXL")? prev.filter(item => item !== "XXL"): [...prev,"XXL"])}>
+            <p className={`${sizes.includes("XXL")? "bg-pink-100" : "bg-slate-200"} px-3 py-1 cursor-pointer`}>XXL</p>
           </div>
         </div>
       </div>
 
       <div className='flex gap-2 mt-2'>
-        <input type="checkbox" id='bestseller' />
+        <input onChange={()=> setBestseller(prev => !prev)} checked={bestseller} type="checkbox" id='bestseller' />
         <label className='cursor-pointer' htmlFor="bestseller">Add to bestseller</label>
       </div>
 
