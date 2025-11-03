919c61488f5cc758b47f29e9f228de1f02bd6180
junaid <junaid.vjunu9895@gmail.com>
2025-11-03 18:03:59 +0530

    order page some update

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+import { backendUrl } from '../App'
 
-const Orders = () => {
+const Orders = ({token}) => {
+
+  const [orders, setOrders] = useState([])
+
+  const fetchAllOrders = async () =>{
+    if(!token) {
+      return null;
+
+    }
+
+    try {
+      const response = await axios.post(backendUrl + '/api/order/list',{},{headers: {token}})
+      console.log(response.data);
+      console.log(token);
+      
+      
+    } catch (error) {
+      
+    }
+  }
+  
+  useEffect(() =>{
+    fetchAllOrders()
+  },[])
+
   return (
-    <div>Orders</div>
+    <div>
+
+    </div>
   )
 }
 
