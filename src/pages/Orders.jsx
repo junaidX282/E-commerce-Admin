7d5bef01826d51409d665feefdf84012a7d2473c
junaid <junaid.vjunu9895@gmail.com>
2025-11-05 17:57:20 +0530

    some update of Orders.jsx

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -3,6 +3,8 @@ import { useEffect } from 'react'
 import { useState } from 'react'
 import axios from 'axios'
 import { backendUrl } from '../App'
+import { toast } from 'react-toastify'
+import { assets } from '../assets/assets'
 
 const Orders = ({token}) => {
 
@@ -16,12 +18,16 @@ const Orders = ({token}) => {
 
     try {
       const response = await axios.post(backendUrl + '/api/order/list',{},{headers: {token}})
-      console.log(response.data);
-      console.log(token);
-      
+      if(response.data.success){
+        setOrders(response.data.orders)
+        console.log(response.data.orders);
+        
+      }else{
+        toast.error(response.data.message)
+      }
       
     } catch (error) {
-      
+      toast.error(error.message)
     }
   }
   
@@ -31,6 +37,45 @@ const Orders = ({token}) => {
 
   return (
     <div>
+
+      <h3>Order Page</h3>
+
+      <div>
+        {
+          orders.map((order, index) =>(
+            <div key={index}>
+              <img src={assets.parcel_icon} alt="" />
+              <div>
+                {
+                  order.items.map((item, index)=>{
+
+                    if(index === order.items.length-1){
+                      return <p key={index}> {item.name} x {item.quantity} <span> {item.size} </span></p>
+                    }
+                    else{
+                      return <p key={index}> {item.name} x {item.quantity} <span> {item.size} </span> , </p>
+                    }
+
+                  })
+                }
+              </div>
+              <p>{order.address.firstName + " " + order.address.lastName}</p>
+              <div>
+                <p>{order.address.street + ","}</p>
+                <p>{order.address.city + ", " + order.address.state + "," +order.address.country + ", " + order.address.zipcode}</p>
+                <p>{order.address.phone}</p>
+              </div>
+
+              <div>
+                <p>Item : {order.items.length}</p>
+                <p>Method : {order.paymentMethod}</p>
+                <p>payment :{ order.payment ? 'Done' : 'pending'}</p>
+                <p>Date : {new Date(order.date). toLocaleDateString()}</p>
+              </div>
+            </div>
+          ))
+        }
+      </div>
 
     </div>
   )
